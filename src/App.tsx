c04e712f20a67fbb11b71252d68ba638c52f7bac
Ahmed Sameh <ahmedsaameh14@gmail.com>
2026-04-19 19:59:51 +0200

    Add Static Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import './App.css'
+import { useState } from "react";
+import type { TaskList } from "./types/Task";
 
 function App() {
+  const [tasks, setTasks] = useState<TaskList>([
+    { id: 1, text: "Task 1", completed: false },
+    { id: 2, text: "Task 2", completed: true },
+  ]);
 
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <h1>To Do App</h1>
+      <div>
+        {tasks.map((task) => (
+          <div key={task.id}>
+            <input type="checkbox" checked={task.completed}/>
+            <span>{task.text}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
